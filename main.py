16b6d47117802beddce2fca40f57a8dd1f08c373
bhubany <yadav.bhuban.by@gmail.com>
2022-06-18 19:30:11 +0545

    Not-found-error-solved

--- main.py
+++ main.py
@@ -11,50 +11,55 @@ page_no=1
 def send_request(page=1, keyword="कोरोना"):
     global count
     url ="https://bg.annapurnapost.com/api/search?title="+str(keyword)+"&page="+str(page)
+    
     try:
         response = requests.get(url)
-        print('Response HTTP Status Code: ', response.status_code)
-        print('Response HTTP Response Body: ', type(response))
-        
-        res = response.json()
-        v=res['data']['items']
+        if(response.status_code ==200):
+            print('Response HTTP Status Code: ', response.status_code)
+            
+            res = response.json()
+            # print("response =", r)
+            v=res['data']['items']
 
-        for val in v[0]:
-            list1.append(val)
-        
-        count += len(v)
-        print(count)
-        
-        for vals in v:
-            temp1 = {}
-            for key in list1:
-                temp1[key] = vals[key]
-                
+            for val in v[0]:
+                list1.append(val)
+            
+            count += len(v)
+            print(count)
+            
+            for vals in v:
+                temp1 = {}
+                for key in list1:
+                    temp1[key] = vals[key]
+                    
+                try:
+                    with open("jsonFile.json", "a", encoding="utf-8") as file:
+                        json.dump(temp1, file, indent=2)
+
+                    # Just For checking Language
+                    with open("jsonFile.txt", "a", encoding="utf-8") as f:
+                        f.write(str(temp1))
+
+                except Exception as err:
+                    print("Error Occurs in (File) : ",err)
+                finally:
+                    file.close()
+                    f.close()
             try:
+                pg={
+                    'page':page
+                }
                 with open("jsonFile.json", "a", encoding="utf-8") as file:
-                    json.dump(temp1, file, indent=2)
-
-                # Just For checking Language
+                        json.dump(pg, file, indent=2)
                 with open("jsonFile.txt", "a", encoding="utf-8") as f:
-                    f.write(str(temp1))
-
-            except Exception as err:
-                print("Error Occurs in FIle : ",err)
-            finally:
-                file.close()
-                f.close()
-        try:
-            pg={
-                'page':page
-            }
-            with open("jsonFile.json", "a", encoding="utf-8") as file:
-                    json.dump(pg, file, indent=2)
-            with open("jsonFile.txt", "a", encoding="utf-8") as f:
-                    f.write("\npage:"+str(page))
-        except Exception as er:
-            print("Error Occurs as : ",er)
+                        f.write("\npage:"+str(page))
+            except Exception as er:
+                print("Error Occurs (Paging) : ",er)
+        else:
+            print("Error Occurs with Status Code: ",response.status_code )
+            exit()
     except Exception as error:
-        print("Error occurs : ", error)
+        print("Error occurs (Requests) : ", error)
     finally:
         pass    
 
@@ -79,7 +84,7 @@ if(existance):
         print("Error Occurs: ",err)
 
 
-# Calling for 3times for 30 articles
+# Calling for 3 times for 30 articles
 while(count<30):
     send_request(page=page_no, keyword=query_str)
     page_no+=1
