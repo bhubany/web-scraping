7720b0e266442bd2ab6c87b4d00455bc9b727aae
bhubany <yadav.bhuban.by@gmail.com>
2022-06-18 15:53:07 +0545

    'searching-and-storing-completed'

---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+from textwrap import indent
+import requests
+
+
+
+list1=[]
+dist1 = {}
+count = 0
+def send_request(page=1, keyword="कोरोना"):
+    global count
+    url ="https://bg.annapurnapost.com/api/search?title="+str(keyword)+"&page="+str(page)
+    try:
+        response = requests.get(url)
+        print('Response HTTP Status Code: ', response.status_code)
+        print('Response HTTP Response Body: ', type(response))
+        
+        res = response.json()
+        v=res['data']['items']
+
+        for val in v[0]:
+            list1.append(val)
+        
+        count += len(v)
+        print(count)
+        
+        for vals in v:
+            temp1 = {}
+            for key in list1:
+                temp1[key] = vals[key]
+                
+            try:
+                with open("jsonFile.json", "a", encoding="utf-8") as file:
+                    json.dump(temp1, file, indent=2)
+
+                # Just For checking Language
+                with open("jsonFile.txt", "a", encoding="utf-8") as f:
+                    f.write(str(temp1))
+
+            except Exception as err:
+                print("Error Occurs in FIle : ",err)
+            finally:
+                file.close()
+                f.close()
+    except Exception as error:
+        print("Error occurs : ", error)
+    finally:
+        pass    
+
+
+query_str = input("Enter Query String: ")
+
+i=1
+while(count<30):
+    send_request(page=i, keyword=query_str)
+    i+=1
