3f71d580d3ba2a8d94276784ea3fa6c338d2705c
bhubany <yadav.bhuban.by@gmail.com>
2022-06-26 15:48:47 +0545

    code cleaning done

--- main.py
+++ main.py
@@ -1,67 +1,35 @@
 import json
-from textwrap import indent
 import requests
-import os
-
 
 list2=[]
-count = 0
-page_no=1
-def send_request(page=1, keyword="कोरोना"):
-    global count, list2
-    url ="https://bg.annapurnapost.com/api/search?title="+str(keyword)+"&page="+str(page)
-    
-    try:
-        response = requests.get(url)
-        if(response.status_code ==200):         
-            res = response.json()
-            v=res['data']['items']                      
-            list2.append(v)
-            count+=1 
 
+def send_request(page=1, keyword="कोरोना", list2=[]):
+    while(page <= 3):
+        url ="https://bg.annapurnapost.com/api/search?title="+str(keyword)+"&page="+str(page)    
+
+        response = requests.get(url)
+        if(response.status_code == 200):         
+            res = response.json()
+            res_data=res['data']['items']                      
+            list2.append(res_data)
+            page+=1
         else:
             print("Error Occurs with Status Code: ",response.status_code )
-            return True     # true that res error occurs
 
-    except Exception as error:
-        print("Error occurs (Requests) : ", error)
-    finally:
-        pass    
+    if(len(list2) != 0):
+        try:
+            with open("jsonFile.json", "w", encoding="utf-8") as file:
+                json.dump({'current_page':(page), 'articles':list2}, file, indent=2)
+        except Exception as err:
+            print("Error Occurs creating Json File : ",err)
+        finally:
+            file.close()
 
-
-query_str = input("Enter Query String: ")
-
-# checking If all 30 articles are fetched from the page
-fileName = "jsonFile.json"
-existance = os.path.exists(fileName)
-
-if(existance):
-    try:
-        f= open('jsonFile.json', 'r', encoding="utf-8")
-        data = json.load(f)
-        previous_page=data['current_page']
-
-        list2.extend(data['articles']) #storing previously fetched Data
-        print("Up to page = "+str(previous_page)+" Has been fetched. Next will start From Page {} (limit is 3) ".format(int(previous_page)+1))
-        count =int(previous_page)
-        page_no=int(previous_page)+1
-    except Exception as err:
-        print("Error Occurs (file exist): ",err)
-
-
-# Calling 3 times for 30 articles in list2
-while(count<3):
-    res_err = send_request(page=page_no, keyword=query_str)
-    if(res_err):
-        break
-    page_no+=1
-
-if(len(list2) != 0):
-    try:
-        with open("jsonFile.json", "w", encoding="utf-8") as file:
-            json.dump({'current_page':(page_no-1), 'articles':list2}, file, indent=2)
-
-    except Exception as err:
-        print("Error Occurs in (File) : ",err)
-    finally:
-        file.close()
+query_str = input("Enter Query String: ") # Taking Keywords
+try:
+    file= open('jsonFile.json', 'r', encoding="utf-8")
+    data = json.load(file)
+    print("Up to page = {} Has been fetched (limit is set to 3).".format(data['current_page']-1))
+    send_request(page=data['current_page'],keyword=query_str, list2=data['articles'])
+except:
+    send_request()
